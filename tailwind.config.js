8586ee3f03310a4876e7cce5be7afa2c01ffb6ff
mukhriddin03 <imuhriddin700@gmail.com>
2025-01-17 19:06:54 +0500

    111

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        'sm360': '360px',
         'xs': '480px' 
-      }
+      },
+      fontFamily: {
+        mavenPro: ['Maven Pro'],
+        roboto: ['Roboto Condensed'],
+        smoochSans: ['Smooch Sans'],
+      },
     },
   },
   plugins: [],
